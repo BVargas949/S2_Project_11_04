fc733797424c43342daef20ccbf7126194d77f7f
Bryan Vargas <BVargas949@west-mec.org>
2019-02-14 18:26:36 -0700

    Renamed files, added javascript in HTML, started creating  variables
    Three base variables were created, the countdown function converts and shares all three variables thought its body.

---
+++ ph_clock.js
@@ -0,0 +1,54 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 4
+
+   Countdown Clock for Ticket Ordering
+   Author: Bryan Vargas
+   Date:  02/14/19
+
+   Filename:   ph_clock.js     
+
+*/
+
+
+var minsLeft = 0;
+var secsLeft = 15;
+var timeLeft = minsLeft * 60 + secsLeft;
+
+var clockID = Countdown();
+
+function Countdown() {
+    var minsLeft = Math.floor(timeLeft / 60);
+    var secsLeft = timeLeft - (minsLeft * 60);
+    var minsString = addLeadingZero(minsLeft);
+    var secsString = addLeadingZero(secsLeft);
+    document.getElementById("minutes").innerHTML = minsString;
+    document.getElementById("seconds").innerHTML = secsString;
+    checkTimer();
+    var timeLeft = -1;
+}
+
+
+
+
+
+/* ------------------------------------------------- */
+
+
+/* The checkTimer() function tests whether there is any time left to make the
+   ticket order. If the time left is 0, the stopClock() function is run;
+   otherwise nothing happens and the program continues to run. */
+
+function checkTimer() {
+    if (timeLeft === 0) stopClock();
+}
+
+
+/* The addLeadingZero() function adds a leading zero to values which are less than 10 */
+
+function addLeadingZero(num) {
+    var numStr = (num < 10) ? ("0" + num) : "" + num;
+    return numStr;
+}
